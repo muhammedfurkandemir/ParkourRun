03430580ca86cbe896e2f8a7666761684529f35b
Furkan Demir <furkan.vty0@gmail.com>
2023-02-08 00:57:54 +0300

    slider tamamlandı
    slider çubuğu eklendi.

--- Assets/Script/Karakter.cs
+++ Assets/Script/Karakter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Karakter : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class Karakter : MonoBehaviour
     public Kamera _Camera;
     public GameObject CharacterFinalPosition;
     bool isCharacterFinalPointOn;
+    public Slider _Slider;
+    public GameObject FinishPoint;
     private void FixedUpdate()
     {
         if (!isCharacterFinalPointOn)
@@ -15,15 +18,25 @@ public class Karakter : MonoBehaviour
         
     }
 
-
+    private void Start()
+    {
+        //slider starting max value 
+        float diff = Vector3.Distance(transform.position, FinishPoint.transform.position);//karakterin başlangıcıdan bitiş triggerına kadar olan mesafeyi alır.
+        _Slider.maxValue = diff;
+    }
     void Update()
     {
         if (isCharacterFinalPointOn)
         {
             transform.position = Vector3.Lerp(transform.position, CharacterFinalPosition.transform.position, .01f);
+            if (_Slider.value != 0)
+                _Slider.value -= 0.005f;
         }
         else
         {
+            //slider diff current value 
+            float diff = Vector3.Distance(transform.position, FinishPoint.transform.position);
+            _Slider.value = diff;//sliderin değerini burada arttırır.
             if (Input.GetKey(KeyCode.Mouse0))
             {
                 if (Input.GetAxis("Mouse X") < 0)
@@ -60,17 +73,17 @@ public class Karakter : MonoBehaviour
             _GameManager.SubCharacters.Add(other.gameObject);           
         }
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)//takılma buglarını yakalar ve belli birim öteleyerek sorunu çözer.
     {
         if (collision.gameObject.CompareTag("Column")||collision.gameObject.CompareTag("PinBox"))
         {
             if (transform.position.x>0)
             {
-                transform.position = new Vector3(transform.position.x - 2f, transform.position.y, transform.position.z);
+                transform.position = new Vector3(transform.position.x - 1f, transform.position.y, transform.position.z);
             }
             else
             {
-                transform.position = new Vector3(transform.position.x + 2f, transform.position.y, transform.position.z);
+                transform.position = new Vector3(transform.position.x + 1f, transform.position.y, transform.position.z);
             }
         }
         else if (collision.gameObject.CompareTag("PropellerNeedle"))
