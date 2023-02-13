c3823797fdf9a3aa77521d97d37267e2a9a6698b
Furkan Demir <furkan.vty0@gmail.com>
2023-02-13 03:12:03 +0300

    ilk aşama tamamlandı

---
+++ Assets/Level_Manager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Furkan;
+
+public class Level_Manager : MonoBehaviour
+{
+    public Button[] Buttons;
+    Mmemory_Managment _MemoryManagment = new Mmemory_Managment();
+    public int level;
+    public Sprite sprite;
+    void Start()
+    {
+        _MemoryManagment.DataSave_Int("LastLevel", level);
+        int currentLevel = _MemoryManagment.DataLoad_Int("LastLevel") - 4;
+        for (int i = 0; i < Buttons.Length; i++)
+        {
+            if (i+1<=currentLevel)
+            {
+                Buttons[i].GetComponentInChildren<Text>().text = (i + 1).ToString();
+
+            }
+            else
+            {
+                Buttons[i].GetComponent<Image>().sprite = sprite;
+            }
+        }
+    }
+
+    
+    public void TurnBack()
+    {
+        SceneManager.LoadScene(0);//ana menu ındex=0
+    }
+}
