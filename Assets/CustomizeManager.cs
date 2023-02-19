25526eb313ecb70d459968387d76fe66e12f29f9
Furkan Demir <furkan.vty0@gmail.com>
2023-02-19 19:01:42 +0300

    .

--- Assets/CustomizeManager.cs
+++ Assets/CustomizeManager.cs
@@ -7,17 +7,22 @@ using Furkan;
 
 public class CustomizeManager : MonoBehaviour
 {
+    [Header("TextField")]
     public TMP_Text puanText;
     public TMP_Text capText;
+    [Header("Caps")]
     public GameObject[] Caps;
+    public Button[] CapButtons;
+    [Header("Sticks")]
     public GameObject[] Sticks;
+    [Header("Materials")]
     public Material[] Costumes;
 
     int capIndex = -1;//sapkanın olmaması durumu için default değer olarak -1 verdik.
     Mmemory_Managment _MemoryManagment = new Mmemory_Managment();
     void Start()
     {
-        _MemoryManagment.DataSave_Int("activeCap", 1);
+        _MemoryManagment.DataSave_Int("activeCap", -1);
         if (_MemoryManagment.DataLoad_Int("activeCap")==-1)
         {
             foreach (var item in Caps)
@@ -32,6 +37,35 @@ public class CustomizeManager : MonoBehaviour
             capIndex = _MemoryManagment.DataLoad_Int("activeCap");
             Caps[capIndex].SetActive(true);
             capText.text = "200";
+        }
+    }
+    public void CapAction_Button(string action)
+    {
+        if (action=="forward")
+        {
+            if (capIndex==-1)
+            {
+                capIndex = 0;
+                Caps[capIndex].SetActive(true);
+            }
+            else
+            {
+                Caps[capIndex].SetActive(false);
+                capIndex++;
+                Caps[capIndex].SetActive(true);
+            }
+            if (capIndex==Caps.Length-1)
+            {
+                CapButtons[1].interactable = false;
+            }
+            else
+            {
+                CapButtons[1].interactable = true;
+            }
+            if (capIndex!=-1)
+            {
+                CapButtons[1].interactable = true;
+            }
         }
     }
 
